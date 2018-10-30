e1e16619bebc1fd198859ed1a344703d9d20ba36
ithuhui <hu545548724@163.com>
2018-10-31 00:27:09 +0800

    doc: add java doc 加点注释

--- src/main/java/com/hui/algorithm/model/Station.java
+++ src/main/java/com/hui/algorithm/model/Station.java
@@ -14,43 +14,108 @@ import java.util.List;
  * @since metrodev2 0.1.0
  */
 public class Station {
+    /**
+     * 站点名字.
+     *
+     * @since hui_project 1.0.0
+     */
     private String name;
+    /**
+     * 所属线路.
+     *
+     * @since hui_project 1.0.0
+     */
     private String line;
+    /**
+     * 相邻连接站点.
+     *
+     * @since hui_project 1.0.0
+     */
     private List<Station> linkStations = new ArrayList<>();
 
+    /**
+     * Gets name.
+     *
+     * @return the name
+     * @since hui_project 1.0.0
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets name.
+     *
+     * @param name the name
+     * @since hui_project 1.0.0
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets line.
+     *
+     * @return the line
+     * @since hui_project 1.0.0
+     */
     public String getLine() {
         return line;
     }
 
+    /**
+     * Sets line.
+     *
+     * @param line the line
+     * @since hui_project 1.0.0
+     */
     public void setLine(String line) {
         this.line = line;
     }
 
+    /**
+     * Gets link stations.
+     *
+     * @return the link stations
+     * @since hui_project 1.0.0
+     */
     public List<Station> getLinkStations() {
         return linkStations;
     }
 
+    /**
+     * Sets link stations.
+     *
+     * @param linkStations the link stations
+     * @since hui_project 1.0.0
+     */
     public void setLinkStations(List<Station> linkStations) {
         this.linkStations = linkStations;
     }
 
+    /**
+     * Instantiates a new Station.
+     *
+     * @param name the name
+     * @param line the line
+     */
     public Station(String name, String line) {
         this.name = name;
         this.line = line;
     }
 
+    /**
+     * Instantiates a new Station.
+     *
+     * @param name the name
+     */
     public Station(String name) {
         this.name = name;
     }
 
+    /**
+     * Instantiates a new Station.
+     */
     public Station (){
 
     }
